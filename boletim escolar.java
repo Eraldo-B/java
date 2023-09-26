549928725d59bdc9e760e94fbe88016b11024479
Eraldo-B <146142524+Eraldo-B@users.noreply.github.com>
2023-09-26 20:05:13 -0300

    Add files via upload

---
+++ boletim escolar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class BoletimEscolar {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner ler = new Scanner(System.in);
+
+        String nomeAluno;
+        float prova1, prova2, prova3;
+        double media;
+
+        System.out.println("Digite o nome do Aluno e depois aperte a tecla enter:.. ");
+        nomeAluno = ler.nextLine();
+        System.out.println("Digite a nota da Prova1 e:depois aperte a tecla enter ");
+        Prova1 = ler.nextDouble();
+        System.out.println("Digite a nota da prova2 e depois aperte a tecla enter: ");
+        prova2 = ler.nextDouble();
+        System.out.println("Digite a nota da prova3 e depois aperte a tecla enter: ");
+        prova3 = ler.nextDouble();
+
+    }
+}
+
+ 
